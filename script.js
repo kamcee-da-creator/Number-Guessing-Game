fdc1e5a16d90850e77eb39a898e44621f6a00e2b
kamcee-da-creator <70220330+kamcee-da-creator@users.noreply.github.com>
2024-09-17 19:06:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,48 +5,48 @@
 //     });
 // import createKindeClient from "@kinde-oss/kinde-auth-pkce-js";
 
-import createKindeClient from './node_modules/@kinde-oss/kinde-auth-pkce-js/dist/kinde-auth-pkce-js.esm.js';
+import createKindeClient from "./node_modules/@kinde-oss/kinde-auth-pkce-js/dist/kinde-auth-pkce-js.esm.js";
 
 async function setupKinde() {
-    const kinde = await createKindeClient({
-        client_id: "7b1d7ef005a64c3e8469a6f61d303a38",
-        domain: "https://kamcee.kinde.com",
-        redirect_uri: "http://localhost:5501"
-    });
+  const kinde = await createKindeClient({
+    client_id: "7b1d7ef005a64c3e8469a6f61d303a38",
+    domain: "https://kamcee.kinde.com",
+    redirect_uri: window.location.origin,
+  });
 
-    document.getElementById('login').addEventListener('click', async () => {
-        await kinde.login();
-    });
+  document.getElementById("login").addEventListener("click", async () => {
+    await kinde.login();
+  });
 
-    document.getElementById('register').addEventListener('click', async () => {
-        await kinde.register();
-    });
+  document.getElementById("register").addEventListener("click", async () => {
+    await kinde.register();
+  });
 
-    document.getElementById('logout').addEventListener('click', async () => {
-        await kinde.logout();
-        toggleView();
-    });
+  document.getElementById("logout").addEventListener("click", async () => {
+    await kinde.logout();
+    toggleView();
+  });
 
-    try {
-        const token = await kinde.getToken();
-        if (token) {
-            toggleView(true);
-        }
-    } catch (error) {
-        console.error('Authentication error:', error);
+  try {
+    const token = await kinde.getToken();
+    if (token) {
+      toggleView(true);
     }
+  } catch (error) {
+    console.error("Authentication error:", error);
+  }
 }
 
 function toggleView(isLoggedIn = false) {
-    const loggedOutView = document.getElementById('logged_out_view');
-    const loggedInView = document.getElementById('logged_in_view');
-    if (isLoggedIn) {
-        loggedOutView.style.display = 'none';
-        loggedInView.style.display = 'block';
-    } else {
-        loggedOutView.style.display = 'block';
-        loggedInView.style.display = 'none';
-    }
+  const loggedOutView = document.getElementById("logged_out_view");
+  const loggedInView = document.getElementById("logged_in_view");
+  if (isLoggedIn) {
+    loggedOutView.style.display = "none";
+    loggedInView.style.display = "block";
+  } else {
+    loggedOutView.style.display = "block";
+    loggedInView.style.display = "none";
+  }
 }
 
 setupKinde();
