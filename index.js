2d2afe78020cf4bc5e5ac8d55976d3a7cf7234e8
kamcee-da-creator <70220330+kamcee-da-creator@users.noreply.github.com>
2024-09-18 09:01:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // import createKindeClient from './node_modules/@kinde-oss/kinde-auth-pkce-js/';
-import createKindeClient from '@kinde-oss/kinde-auth-pkce-js';
+import createKindeClient from '/@kinde-oss/kinde-auth-pkce-js';
 // import createKindeClient from "/node_modules/@kinde-oss/kinde-auth-pkce-js/dist/kinde-auth-pkce-js.esm.js";
 
 async function setupKinde() {
