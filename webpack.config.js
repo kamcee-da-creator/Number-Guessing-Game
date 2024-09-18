49b276c3869abdcdab2196bd89b7f81e3e254bee
kamcee-da-creator <70220330+kamcee-da-creator@users.noreply.github.com>
2024-09-18 09:20:05 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // Your main JavaScript file
+  entry: './index.js', // Your main JavaScript file
   output: {
     path: path.resolve(__dirname, 'dist'), // Output directory
     filename: 'bundle.js' // Output bundle
