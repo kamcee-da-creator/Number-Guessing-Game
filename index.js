aac344a354779177330176981e0e3cfbd5f3ff8b
kamcee-da-creator <70220330+kamcee-da-creator@users.noreply.github.com>
2024-09-18 09:17:01 +0100

    WEBPACK

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // import createKindeClient from './node_modules/@kinde-oss/kinde-auth-pkce-js/';
-import createKindeClient from '/@kinde-oss/kinde-auth-pkce-js';
+import createKindeClient from '@kinde-oss/kinde-auth-pkce-js';
 // import createKindeClient from "/node_modules/@kinde-oss/kinde-auth-pkce-js/dist/kinde-auth-pkce-js.esm.js";
 
 async function setupKinde() {
   const kinde = await createKindeClient({
     client_id: "7b1d7ef005a64c3e8469a6f61d303a38",
     domain: "https://kamcee.kinde.com",
-    redirect_uri: window.location.origin + "/callback",
+    redirect_uri: window.location.origin,
   });
 
   document.getElementById("login").addEventListener("click", async () => {
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Your main JavaScript file
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js' // Output bundle
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
